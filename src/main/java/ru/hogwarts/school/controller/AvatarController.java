6886d8e0bfa3404e1e1fd0b49d39c15bc43a7f72
Black77 <islepuchin@yandex.ru>
2025-02-05 11:24:07 +0300

    Advanced Avatar

---
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -0,0 +1,56 @@
+package ru.hogwarts.school.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.service.AvatarService;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+@RestController
+@RequestMapping("avatar")
+public class AvatarController {
+
+    private final AvatarService avatarService;
+
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "/{studentId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
+        avatarService.uploadAvatar(studentId, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/{studentId}/preview")
+    public ResponseEntity<byte[]> getAvatarPreview(@PathVariable Long studentId) {
+        Avatar avatar = avatarService.findAvatar(studentId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+        return ResponseEntity.ok().headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "/{studentId}/avatar")
+    public void getAvatar(@PathVariable Long studentId, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findAvatar(studentId);
+        Path path = Path.of(avatar.getFilePath());
+        try (InputStream is = new FileInputStream(path.toFile());
+            OutputStream os = response.getOutputStream()) {
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength(avatar.getData().length);
+            response.setStatus(200);
+            is.transferTo(os);
+        }
+    }
+}
